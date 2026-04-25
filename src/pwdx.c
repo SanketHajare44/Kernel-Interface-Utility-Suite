fffccb56b760a07fd9f9b4dd8f7985be1326d436
SanketHajare44 <sankethajare44@gmail.com>
2026-04-25 09:46:12 +0530

    Print working directory

---
+++ src/pwdx.c
@@ -0,0 +1,29 @@
+// Command : pwd(present current working directory)
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+// ./pwdx
+//  argv[0]
+//  argc = 1
+
+/* 
+    Algorithm : 
+
+    Call the system call getcwd() 
+    print the result of getcwd()
+
+*/
+
+int main(int argc, char *argv[])
+{
+    char Buffer[1024] = {'\0'};
+
+    getcwd(Buffer,sizeof(Buffer));
+
+    printf("%s\n",Buffer);
+
+    return 0;
+}
